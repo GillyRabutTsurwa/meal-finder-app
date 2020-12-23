78d9690d38561622e90c90b3c702e32d48567afc
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-12-22 21:04:10 -0600

    completed javascript logic to render random meals

--- js/script.js
+++ js/script.js
@@ -37,16 +37,12 @@ const searchMeal = async (e) => {
       let mealDoesntExistMsg = `<p>There are no search results for ${term}. Try Again</p>`;
       myDOM.resultHeading.innerHTML = data.meals !== null ? mealExistsMsg : mealDoesntExistMsg;
 
-      myDOM.mealsEl.innerHTML = mealsArr
-        .map((currentMealObj) => {
-          // Here was are using destructuring to extract the object properties that we need
-          let { strMealThumb, strMeal, idMeal } = currentMealObj;
-          // And we are using thouse in our makeMealCard().
-          // This makeMealCard() returns the markup that we have been using
-          // So it's the samething as last time. We are just returning a function that returns the markup
-          return makeMealCard(strMealThumb, strMeal, idMeal);
-        })
-        .join("");
+      const mealsArrTemplate = mealsArr.map((currentMealObj) => {
+        let { strMealThumb, strMeal, idMeal } = currentMealObj;
+        return makeMealCard(strMealThumb, strMeal, idMeal);
+      });
+      // NOTE: just making it to another variable (mealsArrTemplate). Still same code base
+      myDOM.mealsEl.innerHTML = mealsArrTemplate.join("");
       console.log(myDOM.mealsEl);
     } catch (error) {
       console.log(`Error fetching meals: ${error}`);
@@ -61,14 +57,56 @@ const searchMeal = async (e) => {
 // Fetch random meal
 // We will refactor this code later on after putting in the logic
 const getRandomMeal = async () => {
+  // clear previous meals and headings; the ones already painted to the DOM
+  myDOM.mealsEl.innerHTML = "";
+  myDOM.resultHeading.innerHTML = "";
   try {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
     const data = await response.json();
     console.log(data);
+    // NOTE: this is an array with one object containing all our info. a bit unorthodox mais its ok
     const meal = data.meals[0];
     console.log(meal);
 
-    const { idMeal, strMeal, strCategory, strArea } = meal;
+    const { idMeal, strMeal, strMealThumb, strCategory, strArea, strInstructions } = meal;
+
+    // I will put this in (a) separate function(s)
+    const ingredients = [];
+
+    for (let i = 1; i <= 20; i++) {
+      // I'm not destructuring these
+      // NOTE: if the current meal ingredient property exists / does not equate to an empty string (I'm assuming an empty string is false)
+      if (meal[`strIngredient${i}`]) {
+        // push the value of the meal ingredient property and the meal measurements property
+        // this is beacause they both have 20 and they both coorelate to one another
+        // so if meal[strIngredient1] = sugar and meal[strMeasure1] = sugar. This is translates to 1 cup of sugar.
+        // Also, if a particular meal only has 14 ingredients, it will consequently only have 14 measurements as well. This is why we are breaking once we start finding empty ingredients and measurement values.
+        //console.log the fetch data if this is still confusing. You will see these properties
+        ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+      } else {
+        break;
+      }
+    }
+
+    // UI markup ici. will put it into its own UI function in the next branch
+    myDOM.single_mealEl.innerHTML = `
+    <div id="single-meal-info" class="single-meal">
+      <h1>${strMeal}</h1>
+      <img src="${strMealThumb}"/>
+      <div class="single-meal-info">
+        ${strCategory ? `<p>${strCategory}</p>` : ""}
+        ${strArea ? `<p>${strArea}</p>` : ""}
+      </div>
+      <div class="main">
+        <p>${strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map((currentIngredient) => `<li>${currentIngredient}</li>`).join("")}
+        </ul>
+      </div>
+    </div>
+  `;
+    console.log(ingredients);
     console.log(`ID # of the meal is ${idMeal}`);
     console.log(`The name of this meal is ${strMeal}`);
     console.log(`The type of dish is ${strCategory}`);
